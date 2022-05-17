e8e088f2ec5a401bd23051151a5725e5e18091c0
Ilya Spivakov <xaker00@gmail.com>
2022-05-16 17:23:16 -0700

    Create tmux-install.sh

---
+++ tmux-install.sh
@@ -0,0 +1,2 @@
+curl https://raw.githubusercontent.com/gpakosz/.tmux/master/.tmux.conf > ~/.tmux.conf
+curl https://raw.githubusercontent.com/xaker00/dotfiles/master/.tmux.conf.local > ~/.tmux.conf.local

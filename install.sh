b640d094e8d5a440efc8e4c16eabaa63c54ed267
Ilya Spivakov <xaker00@gmail.com>
2023-07-11 16:38:59 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,35 @@
 #!/bin/bash -e
 
 pushd $HOME
+
+#
+# tmux
+#
 git clone --depth 1 https://github.com/gpakosz/.tmux.git
 ln -s -f .tmux/.tmux.conf
 cp .tmux/.tmux.conf.local .
 
+#
+# fzf
+#
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
+
+
+#
+# vundle
+#
+git clone --depth 1 https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
+
+#
+# YouCompleteMe
+#
+# Requires:
+#  - cmake
+#  - python3-dev
+#  - build-essential
+#
+$HOME/.vim/bundle/YouCompleteMe/install.py
 
 popd

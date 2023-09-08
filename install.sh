036a103e5b2983398d6d4a906d867398d2fa6352
Ilya Spivakov <xaker00@gmail.com>
2023-09-08 15:55:31 -0700

    Make vim optional

--- install.sh
+++ install.sh
@@ -1,39 +1,62 @@
 #!/bin/bash -e
 
-pushd $HOME
-
 #
 # tmux
 #
-git clone --depth 1 https://github.com/gpakosz/.tmux.git
-ln -s -f .tmux/.tmux.conf
-cp .tmux/.tmux.conf.local .
+install_tmux() {
+    git clone --depth 1 https://github.com/gpakosz/.tmux.git
+    ln -s -f .tmux/.tmux.conf
+    cp .tmux/.tmux.conf.local .
+}
 
 #
 # fzf
 #
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install --all
+install_fzf() {
+    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+    ~/.fzf/install --all
+}
+
 
 #
 # vim
 #
-wget "https://raw.githubusercontent.com/xaker00/dotfiles/master/.vimrc" -O $HOME/.vimrc
+install_vim() {
+    wget "https://raw.githubusercontent.com/xaker00/dotfiles/master/.vimrc" -O $HOME/.vimrc
 
-#
-# vundle
-#
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-vim +PluginInstall +qall
+    #
+    # vundle
+    #
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+    vim +PluginInstall +qall
 
-#
-# YouCompleteMe
-#
-# Requires:
-#  - cmake
-#  - python3-dev
-#  - build-essential
-#
-$HOME/.vim/bundle/YouCompleteMe/install.py
+    #
+    # YouCompleteMe
+    #
+    # Requires:
+    #  - cmake
+    #  - python3-dev
+    #  - build-essential
+    #
+    $HOME/.vim/bundle/YouCompleteMe/install.py
+}
+
+pushd $HOME
+
+case $1 in
+    tmux)
+        install_tmux
+        ;;
+    fzf)
+        install_fzf
+        ;;
+    vim)
+        install_vim
+        ;;
+    *)
+        install_tmux
+        install_fzf
+        ;;
+esac
 
 popd
